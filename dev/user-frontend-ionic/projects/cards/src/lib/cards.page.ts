f2a089262f3ffb3150df5c9e0e447ca3adef7b95
Vladimir Bueb <vbueb@jnesis.com>
2022-12-12 09:49:40 +0000

    feat: MULTI-73 Luminosité

--- dev/user-frontend-ionic/projects/cards/src/lib/cards.page.ts
+++ dev/user-frontend-ionic/projects/cards/src/lib/cards.page.ts
@@ -3,9 +3,10 @@ import { AuthenticatedUser, authenticatedUser$ } from '@ul/shared';
 import { setUserAndCardsData, UserAndCardsData, userAndCardsData$ } from './cards.repository';
 
 import { Network } from '@capacitor/network';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { filter, finalize, first, switchMap } from 'rxjs/operators';
 import { CardsService } from './cards.service';
+import { ScreenService } from './screen.service';
 
 @Component({
   selector: 'app-cards',
@@ -17,13 +18,30 @@ export class CardsPage {
   public authenticatedUser$: Observable<AuthenticatedUser>;
   public userAndCardsData$: Observable<UserAndCardsData> = userAndCardsData$;
   public isLoading = false;
+  private userAndCardsDataSubscription: Subscription;
 
   constructor(
     private cardsService: CardsService,
+    private screenService: ScreenService,
   ) {}
 
   ionViewWillEnter() {
+    this.userAndCardsDataSubscription = userAndCardsData$.subscribe(userAndCardsData => {
+      if (
+        userAndCardsData &&
+        userAndCardsData.cards.staffCard &&
+        !userAndCardsData.errors
+      ) {
+        this.screenService.fullBrightness();
+      }
+    });
+
     this.loadUserCardsData();
+  }
+
+  ionViewWillLeave() {
+    this.userAndCardsDataSubscription?.unsubscribe();
+    this.screenService.restorePreviousBrightness();
   }
 
   private async loadUserCardsData() {
@@ -37,8 +55,8 @@ export class CardsPage {
       filter(authenticatedUser => authenticatedUser != null),
       switchMap(authenticatedUser => this.cardsService.getUserAndCardsData(authenticatedUser.authToken)),
       finalize(() => this.isLoading = false)
-    ).subscribe(data => {
-      setUserAndCardsData(data);
+    ).subscribe(userAndCardsData => {
+      setUserAndCardsData(userAndCardsData);
     });
   }
 }
