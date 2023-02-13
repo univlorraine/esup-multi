0144b0f19e212252f44fbb400d3dfebefc5126b0
Vladimir Bueb <vbueb@jnesis.com>
2023-02-13 11:27:56 +0000

    Fix: MULTI-169 Correction erreur JS non réaffichée

--- dev/user-frontend-ionic/src/zone-flags.ts
+++ dev/user-frontend-ionic/src/zone-flags.ts
@@ -4,3 +4,4 @@
  */
 // eslint-disable-next-line no-underscore-dangle
 (window as any).__Zone_disable_customElements = true;
+(window as any).__zone_symbol__DISABLE_WRAPPING_UNCAUGHT_PROMISE_REJECTION = true;
