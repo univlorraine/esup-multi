de072b300b0f4cffe3460557fe3c617d423a5850
Vladimir Bueb <vbueb@jnesis.com>
2023-04-14 13:26:34 +0000

    Feature: MULTI-229 Corrections fonctionnelles

--- dev/user-frontend-ionic/projects/features/src/lib/features.effects.ts
+++ dev/user-frontend-ionic/projects/features/src/lib/features.effects.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { createEffect, ofType} from '@ngneat/effects';
-import { cleanupPrivateData } from '@ul/shared';
+import { FeaturesService, cleanupPrivateData } from '@ul/shared';
 import { tap } from 'rxjs/operators';
 import { clearFeatures } from '@ul/shared';
+import { Network } from '@capacitor/network';
 
 @Injectable({ providedIn: 'root' })
 export class FeaturesEffects {
@@ -10,5 +11,16 @@ export class FeaturesEffects {
   cleanupPrivateData$ = createEffect(actions => actions.pipe(
     ofType(cleanupPrivateData),
     tap(clearFeatures),
+    tap(async () =>  {
+      // skip if network is not available
+      if (!(await Network.getStatus()).connected) {
+        return;
+      }
+
+      this.featuresService.loadAndStoreFeatures().subscribe();
+    })
   ));
+
+  constructor(private featuresService: FeaturesService) {}
+
 }
