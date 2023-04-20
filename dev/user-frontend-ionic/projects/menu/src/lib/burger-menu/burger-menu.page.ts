636bd0772a9ad60bb612876c11fcef6c3e6180ef
Maxime Damery <mdamery@jnesis.com>
2023-04-20 14:03:36 +0000

    Feat: Multi-211 Menu 3 points

--- dev/user-frontend-ionic/projects/menu/src/lib/burger-menu/burger-menu.page.ts
+++ dev/user-frontend-ionic/projects/menu/src/lib/burger-menu/burger-menu.page.ts
@@ -1,8 +1,10 @@
-import { Component, Inject } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { MenuItem, MenuOpenerService,
-  MenuService as SharedMenuService,  authenticatedUser$, setLanguage, AuthenticatedUser } from '@ul/shared';
-import { Observable } from 'rxjs';
+import { Component, Inject, } from '@angular/core';
+import { App } from '@capacitor/app';
+import { Capacitor } from '@capacitor/core';
+import { MenuItem, MenuOpenerService, MenuService as SharedMenuService,
+  setLanguage, authenticatedUser$, AuthenticatedUser } from '@ul/shared';
+import { Observable, from, of } from 'rxjs';
+import { first, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-menu',
@@ -14,13 +16,14 @@ export class BurgerMenuPage {
   public dynamicMenuItems$: Observable<MenuItem[]>;
   public staticMenuItems$: Observable<MenuItem[]>;
   public languages: Array<string> = [];
+  appVersion$: Observable<string>;
   authenticatedUser$: Observable<AuthenticatedUser>;
 
   constructor(
     @Inject('environment')
     private environment: any,
     private sharedMenuService: SharedMenuService,
-    public menuOpenerService: MenuOpenerService
+    public menuOpenerService: MenuOpenerService,
   ) {
     this.languages = this.environment.languages;
     this.authenticatedUser$ = authenticatedUser$;
@@ -30,10 +33,10 @@ export class BurgerMenuPage {
     this.dynamicMenuItems$ = this.sharedMenuService.burgerMenuItems$.pipe(
       map(menuItems => menuItems.filter(menuItem => menuItem.type === 'dynamic'))
     );
+    this.appVersion$ = !Capacitor.isNativePlatform() ? of(null) : from(App.getInfo()).pipe(map(info => info.version));
   }
 
   useLanguage(language: string): void {
     setLanguage(language);
   }
-
 }
