b0284281c61faebbef4880e474f7a20ac845e21b
Benjamin Lemoine <benjamin.lemoine@univ-lorraine.fr>
2023-06-26 07:49:22 +0000

    feature: MULTI-292 widget derniere actu v2

--- dev/user-frontend-ionic/projects/rss/src/lib/rss.page.ts
+++ dev/user-frontend-ionic/projects/rss/src/lib/rss.page.ts
@@ -1,8 +1,8 @@
-
 import { Component } from '@angular/core';
 import { Browser } from '@capacitor/browser';
 import { Network } from '@capacitor/network';
-import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 import { finalize, map } from 'rxjs/operators';
 import { FeedItem, rssFeed$, setRssFeed } from './rss.repository';
 import { RssService } from './rss.service';
@@ -17,11 +17,12 @@ export class RssPage {
   public rssFeedIsEmpty$: Observable<boolean>;
   public isLoading = false;
   public isContentVisible: boolean[] = [];
-  private rssFeed: FeedItem[];
-
+  public openItemGuid: string;
+  private subscriptions: Subscription[] = [];
 
   constructor(
     private rssService: RssService,
+    private route: ActivatedRoute
   ) {
     this.rssFeedIsEmpty$ = this.rssFeed$.pipe(map(rssFeed => rssFeed.length === 0));
   }
@@ -34,23 +35,18 @@ export class RssPage {
   }
 
   async ionViewWillEnter() {
+    this.subscriptions.push(
+      this.route.queryParams.subscribe(params => {
+        this.openItemGuid = params.guid;
+      })
+    );
     await this.loadRssFeedIfNetworkAvailable();
   }
 
-  public isMediaAnImage(media: any): boolean {
-    const imageTypes: Array<string> = [
-      'image/gif',
-      'image/x-icon',
-      'image/jpeg',
-      'image/png',
-      'image/svg+xml',
-      'image/tiff',
-      'image/webp'
-    ];
-
-    return media.type ? imageTypes.includes(media.type) : false;
+  ionViewDidLeave() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+    this.subscriptions = [];
   }
-
 
   public openContent(index: number) {
     this.isContentVisible[index] = !this.isContentVisible[index];
@@ -62,13 +58,20 @@ export class RssPage {
       return;
     }
     this.isLoading = true;
-    this.rssService.getRssFeed()
-      .pipe(
-        finalize(() => this.isLoading = false)
-      ).subscribe(rssFeed => {
+    this.subscriptions.push(
+      this.rssService.getRssFeed()
+        .pipe(
+          finalize(() => this.isLoading = false)
+        ).subscribe(rssFeed => {
         setRssFeed(rssFeed);
         this.isContentVisible = new Array(rssFeed.length).fill(false);
-      }
-      );
+        if (this.openItemGuid) {
+          const index = rssFeed.findIndex(item => item.guid === this.openItemGuid);
+          if (index >= 0) {
+            this.openContent(index);
+          }
+        }
+      })
+    );
   }
 }
