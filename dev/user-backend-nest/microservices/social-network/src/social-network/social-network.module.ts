db709cf058634a9b4e833187d512167d54a282d3
Vincent Munier <vmunier@jnesis.com>
2023-08-10 14:40:30 +0000

    Update social-network.module.ts

--- dev/user-backend-nest/microservices/social-network/src/social-network/social-network.module.ts
+++ dev/user-backend-nest/microservices/social-network/src/social-network/social-network.module.ts
@@ -3,16 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { SocialNetworkController } from './social-network.controller';
 import { SocialNetworkService } from './social-network.service';
-import * as http from 'http';
-import * as https from 'https';
 
 @Module({
-  imports: [ConfigModule, 
-            HttpModule.register({
-              httpAgent: new http.Agent({ keepAlive: true }),
-              httpsAgent: new https.Agent({ keepAlive: true }),
-            }),
-           ],
+  imports: [ConfigModule, HttpModule],
   controllers: [SocialNetworkController],
   providers: [SocialNetworkService],
 })
