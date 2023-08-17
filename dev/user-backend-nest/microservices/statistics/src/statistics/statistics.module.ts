94ad5e888fb9c4a8bb44e84445e10e085db2aff9
vbueb <vbueb@jnesis.com>
2023-08-17 14:48:18 +0200

    Agentkeepalive appliqué au µ statistics

--- dev/user-backend-nest/microservices/statistics/src/statistics/statistics.module.ts
+++ dev/user-backend-nest/microservices/statistics/src/statistics/statistics.module.ts
@@ -1,11 +1,11 @@
-import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { StatisticsController } from './statistics.controller';
 import { StatisticsService } from './statistics.service';
+import { KeepaliveHttpModule } from '../keepalive-http.module';
 
 @Module({
-  imports: [ConfigModule, HttpModule],
+  imports: [ConfigModule, KeepaliveHttpModule],
   controllers: [StatisticsController],
   providers: [StatisticsService],
 })
