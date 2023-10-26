c54078470ec40b32628e5e0049a32ef8241b9c44
bbrogle <bbrogle@jnesis.com>
2023-10-26 10:03:15 +0200

    Bugfix: MULTI-414: Fixing reauth when db has been cleaned up: no redirect

--- dev/user-frontend-ionic/projects/shared/src/lib/auth/keep-auth.service.ts
+++ dev/user-frontend-ionic/projects/shared/src/lib/auth/keep-auth.service.ts
@@ -7,7 +7,6 @@ import { cleanupPrivateData } from '../shared.actions';
 import { updateAuthToken } from './auth.repository';
 import { AuthenticatedUser, updateUser } from './authenticated-user.repository';
 import { getRefreshAuthToken } from './keep-auth.repository';
-import { NavigationService } from '../navigation/navigation.service';
 
 interface ReauthResult extends AuthenticatedUser {
     authToken: string;
@@ -23,7 +22,6 @@ export class KeepAuthService {
         private environment: any,
         private http: HttpClient,
         private actions: Actions,
-        private navigationService: NavigationService,
     ) {}
 
     public reauthenticateIfAvailable(): Observable<ReauthResult | null> {
@@ -36,7 +34,6 @@ export class KeepAuthService {
                 if (!reauthResult) {
                     // Reauth failed, we activate anonymous mode
                     this.actions.dispatch(cleanupPrivateData({authToken: null}));
-                    this.navigationService.navigateToAuth();
                     return;
                 }
 
