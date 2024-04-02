147ea6239333ea7c4ada8f446f36009ca7623789
Antoine Contoux <contoux1@univ-lorraine.fr>
2024-04-02 14:27:41 +0200

    MULTI-3974: (diffusion) mocks embarques (#13)
    * feat(@multi-backend-mocks): ajout projet mocks anonymisé
    
    (3 commits squashed)
    
    * feat(@multi-backend-mocks): dates dynamiques
    
    * feat(@multi-backend-mocks): mock restaurants
    
    * feat(@multi-backend-mocks): cards
    
    * feat(@multi-backend-mocks): rss
    
    et fix des mocks auth, chatbot et schedule
    
    * feat(@multi-backend-mocks): auth, utilisateurs multiples
    
    3 users : etu, staff et prof
    maj mock card
    
    * feat(@multi-backend-mocks): statistics
    
    + fix suppression notif
    
    * feat(@multi-backend-mocks): clocking
    
    et ajout d'un 2e event mail-calendar
    
    * docs(@multi-backend-mocks): ajout precision sur comptes utilisateurs (mock auth)
    
    * docs: maj libraries
    
    * chore(@multi-backend-mocks): ajout copyright
    
    * refactor(@multi-backend-mocks): changement adresses mail
    
    - ajout schedule-manager
    - business categ UL retires

---
+++ dev/user-backend-mocks/src/auth/auth.route.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright ou © ou Copr. Université de Lorraine, (2022)
+ *
+ * Direction du Numérique de l'Université de Lorraine - SIED
+ *  (dn-mobile-dev@univ-lorraine.fr)
+ * JNESIS (contact@jnesis.com)
+ *
+ * Ce logiciel est un programme informatique servant à rendre accessible
+ * sur mobile divers services universitaires aux étudiants et aux personnels
+ * de l'université.
+ *
+ * Ce logiciel est régi par la licence CeCILL 2.1, soumise au droit français
+ * et respectant les principes de diffusion des logiciels libres. Vous pouvez
+ * utiliser, modifier et/ou redistribuer ce programme sous les conditions
+ * de la licence CeCILL telle que diffusée par le CEA, le CNRS et INRIA
+ * sur le site "http://cecill.info".
+ *
+ * En contrepartie de l'accessibilité au code source et des droits de copie,
+ * de modification et de redistribution accordés par cette licence, il n'est
+ * offert aux utilisateurs qu'une garantie limitée. Pour les mêmes raisons,
+ * seule une responsabilité restreinte pèse sur l'auteur du programme, le
+ * titulaire des droits patrimoniaux et les concédants successifs.
+ *
+ * À cet égard, l'attention de l'utilisateur est attirée sur les risques
+ * associés au chargement, à l'utilisation, à la modification et/ou au
+ * développement et à la reproduction du logiciel par l'utilisateur étant
+ * donné sa spécificité de logiciel libre, qui peut le rendre complexe à
+ * manipuler et qui le réserve donc à des développeurs et des professionnels
+ * avertis possédant des connaissances informatiques approfondies. Les
+ * utilisateurs sont donc invités à charger et à tester l'adéquation du
+ * logiciel à leurs besoins dans des conditions permettant d'assurer la
+ * sécurité de leurs systèmes et/ou de leurs données et, plus généralement,
+ * à l'utiliser et à l'exploiter dans les mêmes conditions de sécurité.
+ *
+ * Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
+ * pris connaissance de la licence CeCILL 2.1, et que vous en avez accepté les
+ * termes.
+ */
+
+const express = require('express');
+const router = express.Router();
+const { authTokenData, userProviderData, errorsData } = require('./auth.mock');
+
+router.post('/', (req, res) => {
+    const { username, password } = req.body;
+    if (username !== password || !userProviderData[username]) {
+        return res.status(401).send(errorsData.unauthorized(username));
+    }
+
+    res.send(authTokenData());
+});
+router.delete('/', (req, res) => res.send(true));
+router.get('/multi-user-provider/:username', (req, res) => {
+    const { username } = req.params;
+    const userData = userProviderData[username];
+    if (!userData) {
+        return res.status(400).send(errorsData.unknownUser);
+    }
+
+    res.json(userData);
+});
+
+module.exports = router;
