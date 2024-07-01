7f4159fa6e65ae85f3bd395fa8b884df5976ba32
Benjamin Lemoine <benjamin.lemoine@univ-lorraine.fr>
2024-07-01 09:47:59 +0200

    Ajout d'une route /healthcheck au serveur de mocks (#49)
    * feat(@multi/mocks-server): ajout d'une route /healthcheck
    
    * fix(@multi/mocks-server): modification routes POST notifications

--- dev/user-backend-mocks/src/notifications/notifications.route.js
+++ dev/user-backend-mocks/src/notifications/notifications.route.js
@@ -42,7 +42,7 @@ const router = express.Router();
 const { notificationsData } = require('./notifications.mock');
 
 router.get('/multi-notification-manager/channels/*', (req, res) => res.send());
-router.post('/multi-notification-manager/notifications/read', (req, res) => res.send());
+router.post('/multi-notification-manager/notifications/*', (req, res) => res.send());
 router.delete('/multi-notification-manager/notifications', (req, res) => res.send());
 router.get('/multi-notification-manager/notifications/*', (req, res) => res.json(notificationsData));
 
