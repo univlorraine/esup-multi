5216cd08fccbec90688b20c9603f299d33b3bbea
Antoine CONTOUX <contoux1@univ-lorraine.fr>
2025-07-22 13:59:39 +0200

    chore(@multi-bakend/clocking): bump dependencies
    @babel/helpers
    https://github.com/advisories/GHSA-968p-4wvh-cqc8
    
    @nestjs/common
    https://github.com/advisories/GHSA-cj7v-w2c7-cp7c
    
    axios
    https://github.com/advisories/GHSA-jr5f-v2jv-69x6
    
    body-parser
    https://github.com/advisories/GHSA-qwcr-r2fm-qrc7
    
    brace-expansion
    https://github.com/advisories/GHSA-v6h2-p8h4-qcjw
    
    cookie
    https://github.com/advisories/GHSA-pxg6-pf52-xh8x
    
    cross-spawn
    https://github.com/advisories/GHSA-3xgq-45jj-v275
    
    form-data
    https://github.com/advisories/GHSA-fjxv-7rqg-78g4
    
    formidable
    https://github.com/advisories/GHSA-75v8-2h7p-7m2m
    
    micromatch
    https://github.com/advisories/GHSA-952p-6rrq-rcjv
    
    multer
    https://github.com/advisories/GHSA-4pg4-qvpc-4q3h
    https://github.com/advisories/GHSA-g5hg-p3ph-g8qg
    https://github.com/advisories/GHSA-fjgf-rc76-4x9p
    
    path-to-regexp
    https://github.com/advisories/GHSA-9wv6-86v2-598j
    https://github.com/advisories/GHSA-rhx6-c78j-4q9w
    
    send
    https://github.com/advisories/GHSA-m6fv-jmcg-4jfg
    
    serialize-javascript
    https://github.com/advisories/GHSA-76p7-773f-r4q5
    
    webpack
    https://github.com/advisories/GHSA-4vvj-4cpr-p986

--- dev/user-backend-nest/microservices/clocking/src/clocking/clocking.module.ts
+++ dev/user-backend-nest/microservices/clocking/src/clocking/clocking.module.ts
@@ -39,12 +39,12 @@
 
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
-import { redisStore } from 'cache-manager-redis-yet';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ClockingService } from './clocking.service';
 import { ClockingController } from './clocking.controller';
 import { KeepaliveHttpModule } from '../keepalive-http.module';
 import { RedisSocket } from '../config/configuration.interface';
+import { createKeyv } from '@keyv/redis';
 
 @Module({
   imports: [
@@ -52,11 +52,14 @@ import { RedisSocket } from '../config/configuration.interface';
     KeepaliveHttpModule,
     CacheModule.registerAsync({
       imports: [ConfigModule],
+      isGlobal: true,
       useFactory: async (configService: ConfigService) => ({
-        store: await redisStore({
-          socket: configService.get<RedisSocket>('redisSocket'),
-          password: configService.get<string>('redisPassword'),
-        }),
+        stores: [
+          createKeyv({
+            socket: configService.get<RedisSocket>('redisSocket'),
+            password: configService.get<string>('redisPassword'),
+          }),
+        ],
         ttl: configService.get<number>('cacheTtl') || 300,
         max: configService.get<number>('cacheMax') || 200,
       }),
