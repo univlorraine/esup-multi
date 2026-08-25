13ac902419d8e3ee89b6f90583f9edad42a985ed
Antoine CONTOUX <contoux1@univ-lorraine.fr>
2026-08-25 17:33:37 +0200

    chore(@multi-backend/card): esm + lint

--- dev/user-backend-nest/microservices/card/src/card/card.service.ts
+++ dev/user-backend-nest/microservices/card/src/card/card.service.ts
@@ -41,10 +41,10 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { RpcException } from '@nestjs/microservices';
-import { catchError, map, Observable } from 'rxjs';
-import { CardProviderApi } from '../config/configuration.interface';
-import { UserCardDto } from './card.dto';
 import type { AxiosError } from 'axios';
+import { catchError, map, Observable } from 'rxjs';
+import { CardProviderApi } from '../config/configuration.interface.js';
+import { UserCardDto } from './card.dto.js';
 
 @Injectable()
 export class CardService {
